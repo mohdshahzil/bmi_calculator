e38e6216fe572b3f0fbb394d03f9acc75068a1d1
mohdshahzil <theshahzil@gmail.com>
2024-05-26 03:36:01 +0530

    box added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,9 +7,43 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BMI Calculator'),
+        title: const Text('Fill in your details!',
+            style: TextStyle(fontWeight: FontWeight.w500)),
       ),
-      body: Text('HEY'),
+      body: Container(
+          margin: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                        margin: const EdgeInsets.all(5),
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[350],
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: const Center(
+                          child: Text('Male'),
+                        )),
+                  ),
+                  Expanded(
+                    child: Container(
+                        margin: const EdgeInsets.all(5),
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[350],
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: const Center(
+                          child: Text('Female'),
+                        )),
+                  ),
+                ],
+              )
+            ],
+          )),
     );
   }
 }
